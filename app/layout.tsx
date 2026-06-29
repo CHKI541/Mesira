0afb2479a9c452bff606125fc2494cb269f5b602
CHKI541 <chki541@users.noreply.github.com>
2026-06-29 16:48:23 -0300

    security: remove keystore from git tracking, fix viewport accessibility (remove userScalable: false)

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,6 @@ export const viewport: Viewport = {
   themeColor: "#0043C6",
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
 };
 
 export const metadata: Metadata = {
